9a770aeb2deb276e44e5a215d3de1e958028fc63
adoufa <arinadolgaya@icloud.com>
2023-01-12 22:00:37 +0300

    3 commit

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
 
 
+posts = [
+    {
+        'author': 'Администратор',
+        'title': 'Это мой первый пост',
+        'content': 'Содержимое первого поста',
+        'date_posted':'12 января 2023 года'
+    },
+    {
+        'author': 'Пользователь №1',
+        'title': 'Это мой второй пост',
+        'content': 'Подробное содержание второго поста',
+        'date_posted':'12 января 2023 года'
+    }
+]
 def home(request):
-    return render(request, 'blog/home.html')
+    context ={
+        'posts': posts
+              }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'О клубе Python Bytes'})
 
 
